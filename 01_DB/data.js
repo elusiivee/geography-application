0cebbc04852ed6308ec18c306a131e1d83ca7ae6
Yegor Maksymchuk <egormaksimchuk@gmail.com>
2018-12-14 08:48:20 +0200

    add dockerfile

---
+++ 01_DB/data.js
@@ -0,0 +1,234 @@
+var data = [
+    {
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },{
+        "question": "",
+        "answers": ["", "", "", ""],
+        "answer": "",
+        "wieght": 1
+    },]
+
+db.questions.insertMany(data);
