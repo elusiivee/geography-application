644a2223d5e50e56ceb414879dec3351a8d26392
Alexander Repeta <axzerk@gmail.com>
2019-01-26 17:33:19 +0200

    Adds cors middleware package, removes body-parser in favor of express.json

--- 02_Server/app.js
+++ 02_Server/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const corsMiddleware = require('cors');
 const router = require('./routers/api');
-
+const errorMiddleware = require('./middleware/errorMiddleware');
 
 // set up express app
 const app = express();
@@ -10,34 +10,25 @@ const app = express();
 // connect to mongodb geography-db
 
 // TODO: вынести секреты в .env?
-mongoose.connect('mongodb://admin:admin@geography-db:27017/geographydb?authSource=admin');
+mongoose.connect(
+  'mongodb://admin:admin@geography-db:27017/geographydb?authSource=admin',
+);
 mongoose.Promise = global.Promise;
 
-//set up static files
+/*
+ * Set up middlewares
+ */
 app.use(express.static('public'));
+app.use(express.json());
+app.use(corsMiddleware());
 
-// use body-parser middleware
-
-// TODO: в 4+ express есть прослоайки всроенные, bodyParser уже не нужен
-app.use(bodyParser.json());
-
-// initialize routes
+// Initialize routes
 app.use('/api', router);
 
-// error handling middleware
-app.use(function(err, req, res) {
-  console.log(err); // to see properties of message in our console
-  res.status(422).send({ error: err.message });
-});
+// Error handling middleware
+app.use(errorMiddleware);
 
-// TODO: есть пакет который CORS покрывает, там внутри прослоечка
-app.use(function(req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-});
-
-
-// listen for requests
+// Listen for requests
 app.listen(process.env.port || 4000, function() {
   console.log('Server started and wait requests');
 });
