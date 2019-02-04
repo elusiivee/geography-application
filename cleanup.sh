1d2de6b8eee8f9d4f1464ed975716a647da90ef6
Yegor Maksymchuk <ymaksymc@redhat.com>
2019-02-04 09:13:28 +0200

    cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
+set -e
 docker stop $(docker ps -qa) \
-    && docker rm -f $(docker ps -qa) \
-    && docker rmi -f $(docker ps -qa) \
-    && docker network prune
+    || docker rm -f $(docker ps -qa) \
+    || docker rmi -f $(docker images -qa) \
+    || docker network prune
--- run-application.sh
+++ run-application.sh
@@ -12,7 +12,7 @@ docker build -f Dockerfile.single -t geo-srv .
 docker run -d -it -p 4000:4000 --name=geography-server --network geo-net geo-srv
 docker start geography-server
 
-cd ../03_Client/geo-ui
+cd ../03_Client
 docker build -t geo-ui .
 docker run -d -it -p 3000:3000 --name=geography-frontend --network geo-net geo-ui
 docker start geography-frontend
