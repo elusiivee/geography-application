402ef97c0930995f8d06b32d6c4a739cc2613091
Maksym Khalin <xadmax@gmail.com>
2023-12-06 16:21:22 +0100

    Update create-docker-network.sh. Unused docker start commented

--- create-docker-network.sh
+++ create-docker-network.sh
@@ -5,7 +5,7 @@ docker network create geo-net
 cd 01_DB
 docker build -f Dockerfile.single -t geo-db .
 docker run -d -it -p 27017:27017 -v ./data:/tmp --name=geography-db --network geo-net geo-db
-docker start geography-db
+#docker start geography-db
 
 # Give time for the DB to start
 sleep 15s
@@ -18,7 +18,7 @@ docker run -d -it -p 4000:4000 --name=geography-server --network geo-net geo-srv
 cd ../03_Client/
 docker build -t geo-front .
 docker run -d -it -p 3000:3000 --name=geography-frontend --network geo-net geo-front
-docker start geography-frontend
+#docker start geography-frontend
 
 sleep 5s
 
